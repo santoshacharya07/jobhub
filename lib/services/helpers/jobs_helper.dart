53541344c259a44c106641e0ffd52110edecbff4
santoshacharya07 <santoshacharya747@gamil.com>
2023-06-20 23:02:34 +0545

    horizontal tile api connected

--- lib/services/helpers/jobs_helper.dart
+++ lib/services/helpers/jobs_helper.dart
@@ -1,6 +1,30 @@
 import 'package:http/http.dart' as https;
+import 'package:jobhub/models/response/jobs/jobs_response.dart';
+
+import '../config.dart';
 
 class JobsHelper {
   static var client = https.Client();
 
+  static Future<List<JobsResponse>> getJobs() async {
+    Map<String, String> requestHeaders = {
+      'Content-Type': 'application/json',
+    };
+
+    var url = Uri.https(Config.apiUrl, Config.jobs);
+
+    var response = await client.get(
+      url,
+      headers: requestHeaders,
+    );
+
+    if (response.statusCode == 200) {
+      var jobList = jobsResponseFromJson(response.body);
+      print("Successfulllll");
+      return jobList;
+    } else {
+      print("Failedddd");
+      throw Exception("Failed to get the jobs");
+    }
+  }
 }
--- lib/views/ui/homepage.dart
+++ lib/views/ui/homepage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:jobhub/controllers/exports.dart';
 import 'package:jobhub/views/common/app_bar.dart';
 import 'package:jobhub/views/common/drawer/drawer_widget.dart';
 import 'package:jobhub/views/common/exports.dart';
@@ -9,8 +10,10 @@ import 'package:jobhub/views/common/height_spacer.dart';
 import 'package:jobhub/views/common/search.dart';
 import 'package:jobhub/views/common/vertical_tile.dart';
 import 'package:jobhub/views/ui/jobs/job_page.dart';
+import 'package:jobhub/views/ui/jobs/widgets/horizontal_shimmer.dart';
 import 'package:jobhub/views/ui/jobs/widgets/horizontal_tile.dart';
 import 'package:jobhub/views/ui/search/searchpage.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -41,59 +44,77 @@ class _HomePageState extends State<HomePage> {
               child: DrawerWidget(),
             ),
           )),
-      body: SafeArea(
-          child: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20.w),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const HeightSpacer(size: 10),
-              Text(
-                "Search \nFind & Apply",
-                style: appstyle(40, Color(kDark.value), FontWeight.bold),
+      body: Consumer<JobsNotifier>(
+        builder: (context, jobNotifier, child) {
+          jobNotifier.getJobs();
+          return SafeArea(
+              child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.w),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const HeightSpacer(size: 10),
+                  Text(
+                    "Search \nFind & Apply",
+                    style: appstyle(40, Color(kDark.value), FontWeight.bold),
+                  ),
+                  const HeightSpacer(size: 40),
+                  SearchWidget(
+                    onTap: () {
+                      Get.to(() => const SearchPage());
+                    },
+                  ),
+                  const HeightSpacer(size: 30),
+                  HeadingWidget(
+                    text: "Popular Jobs",
+                    onTap: () {},
+                  ),
+                  const HeightSpacer(size: 15),
+                  SizedBox(
+                      height: heiGht * 0.28,
+                      child: FutureBuilder(
+                          future: jobNotifier.jobList,
+                          builder: (context, snapshot) {
+                            if (snapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              return const HorizontalShimmer();
+                            } else if (snapshot.hasError) {
+                              return Text("${snapshot.error}");
+                            } else {
+                              final jobs = snapshot.data;
+                              return ListView.builder(
+                                  itemCount: jobs!.length,
+                                  scrollDirection: Axis.horizontal,
+                                  itemBuilder: (context, index) {
+                                    final job = jobs[index];
+                                    return JobHorizontalTile(
+                                      onTap: () {
+                                        Get.to(() => const JobPage(
+                                              title: "Facebook",
+                                              id: "12",
+                                            ));
+                                      },
+                                      job: job,
+                                    );
+                                  });
+                            }
+                          })),
+                  const HeightSpacer(size: 20),
+                  HeadingWidget(
+                    text: "Recently Posted",
+                    onTap: () {},
+                  ),
+                  const HeightSpacer(size: 20),
+                  VerticalTile(
+                    onTap: () {},
+                  )
+                ],
               ),
-              const HeightSpacer(size: 40),
-              SearchWidget(
-                onTap: () {
-                  Get.to(() => const SearchPage());
-                },
-              ),
-              const HeightSpacer(size: 30),
-              HeadingWidget(
-                text: "Popular Jobs",
-                onTap: () {},
-              ),
-              const HeightSpacer(size: 15),
-              SizedBox(
-                height: heiGht * 0.28,
-                child: ListView.builder(
-                    itemCount: 4,
-                    scrollDirection: Axis.horizontal,
-                    itemBuilder: (context, index) {
-                      return JobHorizontalTile(
-                        onTap: () {
-                          Get.to(() => const JobPage(
-                                title: "Facebook",
-                                id: "12",
-                              ));
-                        },
-                      );
-                    }),
-              ),
-              const HeightSpacer(size: 20),
-              HeadingWidget(
-                text: "Recently Posted",
-                onTap: () {},
-              ),
-              const HeightSpacer(size: 20),
-              VerticalTile(
-                onTap: () {},
-              )
-            ],
-          ),
-        ),
-      )),
+            ),
+          ));
+        },
+      ),
     );
   }
 }
